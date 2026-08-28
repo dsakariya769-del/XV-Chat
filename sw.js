3ff3bae4a5b98890c53a2a19745765db4facfb70
dsakariya769-del <dsakariya769@gmail.com>
2026-08-28 15:40:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='xv-chat-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./firebase-config.js','./xv-logo.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
